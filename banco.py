afe79e487ea822e3fe0e8c1ed593ff9a0981c7c2
GabrielLovato <gabriellcc20@gmail.com>
2025-05-09 18:11:45 -0300

    add: inserir artista e album e um menu interativo

--- banco.py
+++ banco.py
@@ -17,6 +17,44 @@ try:
     cursor = conn.cursor()
     print("conexao realizada")
 
+    
+    def inserir_artista():
+        nome = input("Digite o nome do artista: ")
+        pais = input("Digite o país do artista: ")
+        generoMusical = input("Digite o genero musical do artista: ")
+
+        sql = "INSERT INTO Artista (nome, pais, genero_musical_principal) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (nome, pais, generoMusical))
+        conn.commit()
+    
+    def inserir_album():
+        titulo = input("Digite o título do álbum: ")
+        data_lancamento = input("Digite a data de lançamento (YYYY-MM-DD): ")
+        artista_id = input("Digite o ID do artista: ")
+
+        if not artista_id:
+            print("Erro: artista_id é obrigatório.")
+            return
+        
+        sql = "INSERT INTO Album (titulo, data_lancamento, artista_id) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (titulo, data_lancamento, artista_id))
+        conn.commit()
+        
+
+    while True:
+        print("\n-----MENU-----")
+        print("1 - Inserir Artista")
+        print("2 - Inserir Album")
+        opcao = input("Digite a opção que deseja inserir: ")
+        if(opcao == "1"):
+            inserir_artista()
+        elif(opcao == "2"):
+            inserir_album()
+        elif opcao == "0":
+            break
+        else:
+            print("Opcao invalida")
+
     conn.close()
     print("conexao fechada")
 
